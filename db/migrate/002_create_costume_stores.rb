fc15b2a14cc741256b226d8a9101b2298b76af5f
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-22 17:38:21 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integer :employees
+      t.boolean :in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+end
